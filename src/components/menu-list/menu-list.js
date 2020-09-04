d6848ace4119a6f9994c7ddaa433770c04d7dd50
roman.kostin <rko43@mail.ru>
2020-09-04 13:23:19 +0300

    change db.json/menuList

--- src/components/menu-list/menu-list.js
+++ src/components/menu-list/menu-list.js
@@ -1,58 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MenuListItem from '../menu-list-item';
-import WithRestoService from '../hoc';
-import { connect } from 'react-redux';
-import { menuLoaded, menuRequseted, menuError } from '../../actions/';
+// import WithRestoService from '../hoc';
+// import { connect } from 'react-redux';
+// import { menuLoaded, menuRequseted, menuError } from '../../actions/';
 import Spinner from '../spinner/';
 import Error from '../error';
 
 import './menu-list.scss';
 
-class MenuList extends Component {
-  componentDidMount() {
-    this.props.menuRequseted();
-
-    const { RestoService } = this.props;
-    RestoService.getMenuItems().then((res) => this.props.menuLoaded(res));
+const MenuList = ({ menuItems, loading, error }) => {
+  if (loading) {
+    return <Spinner />;
   }
 
-  componentDidCatch() {
-    this.props.menuError();
+  if (error) {
+    return <Error />;
   }
 
-  render() {
-    const { menuItems, loading, error } = this.props;
-
-    if (loading) {
-      return <Spinner />;
-    }
-
-    if (error) {
-      return <Error />;
-    }
-
-    return (
-      <ul className="menu__list">
-        {menuItems.map((menuItem) => {
-          return <MenuListItem key={menuItem.id} menuItem={menuItem} />;
-        })}
-        ;
-      </ul>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    menuItems: state.menu,
-    loading: state.loading,
-    error: state.error,
-  };
-};
-const mapDispatchToProps = {
-  menuLoaded,
-  menuRequseted,
-  menuError,
+  return (
+    <ul className="menu__list">
+      {menuItems.map((menuItem) => {
+        return <MenuListItem key={menuItem.id} menuItem={menuItem} />;
+      })}
+    </ul>
+  );
 };
 
-export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(MenuList));
+export default MenuList;
--- src/components/pages/main-page.js
+++ src/components/pages/main-page.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MenuList from '../menu-list';
+import { connect } from 'react-redux';
+import { menuLoaded, menuRequseted, menuError } from '../../actions/';
 
-const MainPage = () => {
-    return (
-        <MenuList/>
-    )
+import WithRestoService from '../hoc/';
+
+class MainPage extends Component {
+  componentDidMount() {
+    this.props.menuRequseted();
+
+    const { RestoService } = this.props;
+    RestoService.getMenuItems().then((res) => this.props.menuLoaded(res));
+  }
+
+  componentDidCatch() {
+    this.props.menuError();
+  }
+
+  render() {
+    const { menuItems, loading, error } = this.props;
+
+    return <MenuList menuItems={menuItems} loading={loading} error={error} />;
+  }
 }
 
-export default MainPage;
+const mapStateToProps = (state) => {
+  return {
+    menuItems: state.menu,
+    loading: state.loading,
+    error: state.error,
+  };
+};
+const mapDispatchToProps = {
+  menuLoaded,
+  menuRequseted,
+  menuError,
+};
+
+export default WithRestoService()(connect(mapStateToProps, mapDispatchToProps)(MainPage));
